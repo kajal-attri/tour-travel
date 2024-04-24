944009a06237a3edf3893fbef01866b3e890d877
kajal <161615944+kajal-attri@users.noreply.github.com>
2024-04-24 12:06:48 +0530

    about html

---
+++ about.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+
+  <!-- swiper css link -->
+  <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <!-- font awesome cdn link -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+   <!-- custom css file link -->
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+  
+<!-- header section starts -->
+
+<section class="header">
+
+<a href="home.php" class="logo">travel.</a>
+
+<nav class="navbar">
+    <a href="home.php">home</a>
+    <a href="about.php">about</a>
+    <a href="package.php">package</a>
+    <a href="book.php">book</a>
+</nav>
+
+<div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url('images/about\ background.jpg') no-repeat">
+    <h1>about us</h1>
+</div>
+
+<!-- about section starts -->
+
+<section class="about">
+
+<div class="image">
+  <img src="images/trip planning.jpg" alt="">
+</div>
+
+<div class="content">
+  <h3>why choose us?</h3>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea perferendis nam facere ipsum provident! Odio, vitae consequatur! Possimus, fugiat culpa.</p>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias quis corporis rem commodi, ea molestiae debitis sunt nobis ut est!</p>
+<div class="icons-container">
+  <div class="icons">
+    <i class="fas fa-map"></i>
+    <span>top destinations</span>
+  </div>
+  <div class="icons">
+    <i class="fas fa-hand-holding-usd"></i>
+    <span>affordable price</span>
+  </div>
+  <div class="icons">
+    <i class="fas fa-headset"></i>
+    <span>24/7 guide service</span>
+  </div>
+</div>
+</div>
+
+</section>
+
+<!-- about section ends-->
+
+<!-- review section starts -->
+
+<section class="reviews">
+    <h3>clients reviews</h3>
+<div class="swiper reviews-slider">
+
+<div class="swiper-wrapper">
+
+<div class="swiper-slide slide">
+<div class="stars">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui voluptates est temporibus dolores vel, repellendus iste porro distinctio ut minima!</p>
+<h3>Mahi Bhai</h3>
+<span>traveler</span>
+<img src="images/mahi bhai.webp" alt="">
+</div>
+
+<div class="swiper-slide slide">
+<div class="stars">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem sit sapiente aliquid assumenda laboriosam quisquam molestiae officia itaque voluptas! Culpa distinctio vel voluptates nihil laudantium recusandae deleniti saepe nostrum? Facere sequi assumenda, reprehenderit alias unde amet nobis fuga voluptatum tenetur.</p>
+<h3>kaju</h3>
+<span>traveler</span>
+<img src="images/kaju.avif" alt="">
+</div>
+
+<div class="swiper-slide slide">
+<div class="stars">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et hic ab ratione est nobis blanditiis necessitatibus suscipit aspernatur quae quisquam?</p>
+<h3>Russell</h3>
+<span>traveler</span>
+<img src="images/russel.webp" alt="">
+</div>
+
+<div class="swiper-slide slide">
+<div class="stars">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae a corrupti quibusdam reiciendis itaque error, est ut minus odio nesciunt animi. Quasi sapiente ipsa soluta voluptatibus reiciendis saepe odio debitis vel omnis, dignissimos commodi nam, ratione laboriosam sequi animi deserunt?</p>
+<h3>Ellena</h3>
+<span>traveler</span>
+<img src="images/passport.jpg" alt="">
+</div>
+
+<div class="swiper-slide slide">
+<div class="stars">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore quam deleniti facere velit cum totam ut quaerat necessitatibus, commodi temporibus.</p>
+<h3>Aman</h3>
+<span>traveler</span>
+<img src="images/aman,.jpg" alt="">
+</div>
+
+<div class="swiper-slide slide">
+<div class="stars">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque, voluptatum facere modi laborum voluptatibus in perferendis? Nobis nulla nisi, similique et illum ipsa nihil aspernatur cum? Cupiditate deleniti laudantium ipsum nobis minima. Facere officia quae impedit, blanditiis sit laboriosam voluptas.</p>
+<h3>rohit sharma</h3>
+<span>traveler</span>
+<img src="images/rohit sharma.avif" alt="">
+</div>
+
+</div>
+
+</div>
+
+</section>
+
+
+
+
+
+<!-- review section ends -->
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts -->
+
+<section class="footer">
+
+<div class="box-container">
+
+<div class="box">
+  <h3>quick links</h3>
+    <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+    <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+    <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+    <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+
+</div>
+
+<div class="box">
+  <h3>extra links</h3>
+    <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+    <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+    <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+    <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+  </div>
+
+  <div class="box">
+  <h3>contact info</h3>
+    <a href="#"> <i class="fas fa-phone"></i> +91-6005765653</a>
+    <a href="#"> <i class="fas fa-phone"></i> +91-6005465653</a>
+    <a href="#"> <i class="fas fa-envelope"></i> singhrohit4549@gmail.com</a>
+    <a href="#"> <i class="fas fa-map"></i> Gurgaon, India - 122202</a>
+  </div>
+
+  <div class="box">
+    <h3>follow us</h3>
+    <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+    <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+    <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+    <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+  </div>
+
+</div>
+
+<div class="credit"> created by <span>Mr. Rohit Singh</span> | all right reserved!</div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+<!-- swiper js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+<script src="script.js"></script>
+
+</body>
+</html>
